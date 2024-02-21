8f6a99a9a0300309080932ccdc510e1912c16034
JaredPetersen2003 <jared.petersen9@gmail.com>
2024-02-21 15:22:14 +0200

    Added UDP listener in client

--- client.py
+++ client.py
@@ -1,13 +1,37 @@
 from socket import *
+import threading
 
 #configure client
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+DISCONNECT = 'DISCONNECT'
 
 message = 'login'
+
+
+
 clientSocket.send(message.encode())
 
+
+def udp_start():
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(('', serverPort))
+    print('The server is ready to receive')
+    udp = threading.Thread(target=udp_listner, args=(serverSocket,))
+    udp.start()
+
+def udp_listner(serverSocket):
+    while True:
+        msg, clientAddress = serverSocket.recvfrom(2048)
+        modifiedMessage = message.decode().upper()
+        serverSocket.sendto(modifiedMessage.encode(),
+        clientAddress)
+
+udp_start()
+
+clientSocket.close()
 
 
