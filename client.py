f9fbe1929dc71bb9987d30698f0413fee62a367d
JaredPetersen2003 <jared.petersen9@gmail.com>
2024-02-22 20:57:16 +0200

    Added Login and made progress on disconnect

--- client.py
+++ client.py
@@ -1,36 +1,64 @@
+import random
 from socket import *
 import threading
 
 #configure client
 serverName = 'localhost'
-serverPort = 12000
+serverPort = 12001
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 DISCONNECT = 'DISCONNECT'
-
+connected = True
 message = 'login'
 
 
-
-clientSocket.send(message.encode())
-
-
-def udp_start():
-    serverPort = 12001
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
-    serverSocket.bind(('', serverPort))
-    print('The server is ready to receive')
-    udp = threading.Thread(target=udp_listner, args=(serverSocket,))
-    udp.start()
+    
+#send message
+def send_message():
+    global connected 
+    while connected:
+        message = input()
+        clientSocket.send(message.encode())
+        if message == DISCONNECT:
+            connected = False
+            break
+    serverSocket.close()
+    clientSocket.close()
+    print("Connection closed")
 
 def udp_listner(serverSocket):
-    while True:
+    global connected 
+    while connected:
         msg, clientAddress = serverSocket.recvfrom(2048)
         print(msg.decode())
+    print("UDP listener closed")
         
+def tcp_listner():
+    global connected 
+    while connected:
+        message = clientSocket.recv(1024)
+        print(message.decode())
+    print("TCP listener closed")
 
-udp_start()
+def tcp_start():
+    tcp = threading.Thread(target=tcp_listner)
+    tcp.start()
+    
 
-clientSocket.close()
+# Configure UDP server
+serverPort = random.randint(8000, 12003)
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print('The server is ready to receive')
+udp = threading.Thread(target=udp_listner, args=(serverSocket,))
+udp.start()
+
+
+
+tcp_start()
+
+send_message()
+
+
 
 
--- server.py
+++ server.py
@@ -2,28 +2,57 @@ from socket import *
 import selectors
 
 
+def handle_login(conn, mask):
+    data = conn.recv(1024)
+    if data:
+        print(data.decode()[:3])
+        if data.decode()[:5] == "login":
+            print("Login request received")
+            # TODO implement login
+            conn.send("Login successful".encode())
+            sel.unregister(conn)
+            sel.register(conn, selectors.EVENT_READ, read)
+        if data.decode()[:5] == "regis":
+            print("Registration request received")
+            # TODO implement registration
+            conn.send("Registration successful".encode())
+            sel.unregister(conn)
+            sel.register(conn, selectors.EVENT_READ, read)
+    
+
 # Accept incoming connections
 def accept(sock, mask):
     conn, addr = sock.accept()  
+    clients.append(conn)
+    
     print("Client " + str(addr) + " connected!")
     print("Active client" + str(clients))
-    clients.append(conn)
+    # TODO better way to handle an active list of clients
     conn.setblocking(False)
     # Register the socket to be monitored with the selector
-    sel.register(conn, selectors.EVENT_READ, read)
+    sel.register(conn, selectors.EVENT_READ, handle_login)
 
 # Read incoming messages
 def read(conn, mask):
     data = conn.recv(1024)  
     if data:
         print(data)
-        UDPSocket.sendto(data, ('localhost', 12001))
-        
+        if data.decode() == "DISCONNECT":
+            print("Client " + str(conn.getpeername()) + " disconnected!")
+            sel.unregister(conn)
+            clients.remove(conn)
+            conn.close()
+            
+        #Send list of clients
+        if data.decode() == "GETC":
+            print("GET request received")
+            active_clients = [str(client.getpeername()) for client in clients if client.fileno() != conn.fileno()]
+            conn.send("\n".join(active_clients).encode())
 
 
 #Configure Server
 print("Setting up server")
-serverPort = 12000
+serverPort = 12001
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
